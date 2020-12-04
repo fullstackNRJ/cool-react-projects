899e3c427efa197cafbf51becb3583bcce8484c4
devnrj07 <niraj.mukta123@gmail.com>
2020-12-04 13:10:12 +0530

    added storeProvider

--- rick-morty-app/src/App.tsx
+++ rick-morty-app/src/App.tsx
@@ -1,32 +1,46 @@
 import React from 'react';
 import './App.css';
-import { Store } from './Store';
+import Header from './componets/Header';
+import Card from './componets/Card';
+import { Store, StoreProvider } from './Store';
+import { Episode } from './componets/Interfaces';
 
 
 
 
 function App(): JSX.Element {
-  const state = React.useContext(Store);
+  const { state, dispatch } = React.useContext(Store);
 
   const fetchAllEpisodes = async () => {
     const URL = 'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes'
     const data = await (await fetch(URL)).json()
-/*     return dispatch({
+    return dispatch({
       type: 'FETCH_DATA',
       payload: data._embedded.episodes
-    }) */
+    })
   }
 
   React.useEffect(() => {
     state.episodes.length === 0 && fetchAllEpisodes()
   })
 
-  console.log('<<<state>>>', state)
+  const HandleFavBtn = (episode: Episode) => {
+    console.log('<<Episode clicked>>', episode)
+  }
   return (
     <React.Fragment>
-      <h1>Rick and Morty App</h1>
-      <p>You can add your favourite episodes</p>
+      <div>
+        <Header />
+        <section>
+          {state.episodes.map((episode: Episode, key: number) => {
+            return (<Card key={key} episode={episode} toggleFav={HandleFavBtn} />)
+          })}
+        </section>
+      </div>
+
+
     </React.Fragment>
+
   );
 }
 
